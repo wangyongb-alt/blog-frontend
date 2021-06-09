2a832d12053982c7c7568969f0b7a1e0e3ced14a
wyb <wangyongbo@youjiajk.com>
2021-06-09 13:53:02 +0800

    xiug

--- src/pages/Post.vue
+++ src/pages/Post.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <!-- Page Header-->
-    <header class="masthead" :style="{backgroundImage: `url(${GRIDSOME_API_URL + $page.post.cover.url})`}">
+    <header class="masthead">
         <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
